1da4a94aff7cbae6dccb676427adbfda4b9b2825
Roman Storchak <webprogramingroman@gmail.com>
2025-06-21 16:29:13 +0200

    plugin init

---
+++ plugins/blocks-gamestore/build/block-contact/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '507cb02613d16a37e0df');
---
+++ plugins/blocks-gamestore/build/block-hero/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '80236a94e11612754184');
